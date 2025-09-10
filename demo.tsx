2110622cac86dccea3c0eff8bf7d48fff5f811d2
Phạm Danh Hưởng <danhhuong246@gmail.com>
2025-09-10 09:21:24 +0700

    refactor: update ReactDOM import and rendering method in demo file

--- demo.tsx
+++ demo.tsx
@@ -1,15 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import ExamplePage from "./src/ExamplePage";
 
 // This is a demo file to showcase the ExamplePage component
 // You can use this to test your shopping cart library components
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
   <React.StrictMode>
     <ExamplePage />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // Alternative: You can also import individual components
